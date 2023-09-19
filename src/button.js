289c79819078b914c03660cdfbd2687496d57204
som3th1n <144984019+som3th1n@users.noreply.github.com>
2023-09-19 21:23:32 +0700

    Add files via upload

---
+++ src/button.js
@@ -0,0 +1,6 @@
+function showNotes() {
+    document.getElementById("giftbox-img").style.display = "none";
+    document.getElementById("paper").style.display = "none";
+    document.getElementById("wish").style.display = "block";
+    document.getElementById("body").style.padding = "10px";
+} 
